f227b86222e9d543a0f7a2d0889cb0ec23ae256b
Leon-aka-10 <chuboy_10@yahoo.com>
2022-06-28 21:12:10 +0000

    commit sept

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for nums in range(0, 90):
         if nums != 89:
             print("{0:02d}, ".format(nums), end='')
     else:
-            print("{0:02d}".format(nums))
+        print("{0:02d}".format(nums))
