140bd6f00440e76fed9097dfa377c3eca1328785
Leon-aka-10 <chuboy_10@yahoo.com>
2022-07-04 10:22:56 +0000

    commit six

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -7,6 +7,6 @@ def no_c(my_string):
     for x in range(len(my_string)):
         if (my_string[x] == 'c' or my_string[x] == 'C'):
             new_string = new_string[:(x - y)] + my_string[(x + 1):]
-           y += 1
+            y += 1
 
     return (new_string)
