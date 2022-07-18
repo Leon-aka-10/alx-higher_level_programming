a2e51c093f9536d92ff08052248890cdb1b3dc99
Leon-aka-10 <chuboy_10@yahoo.com>
2022-07-18 17:13:25 +0000

    commit UN

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     for alphnum in range(x):
         try:
             print("{}".format(my_list[alphnum]), end='')
-        except TypeError:
+        except ValueError:
             break
         else:
             element += 1
